ccf4ace393d92894f2fe89109edd9bb4924672bb
Charles PIGNEROL <>
2025-10-28 14:28:19 +0100

    Version 8.8.3. Qt 6.10.0 / VTK 9.2.5 port. The port appears to be working fine in 9.5.2 but is not validated with Qt 6.10.0 because the graphical window is often black. Works well in Qt 6.6.1/VTK 9.2.5.

--- src/viewpoint/viewpoint.cpp
+++ src/viewpoint/viewpoint.cpp
@@ -9,6 +9,7 @@
 #include <TkUtil/Exception.h>
 
 #include <QApplication>
+#include <QSurfaceFormat>
 
 #include <iostream>
 
@@ -22,6 +23,22 @@ int main (int argc, char* argv[])
 	try
 	{
 		QApplication*	application	= new QApplication (argc, argv);
+#if (VTK_MAJOR_VERSION >= 9)
+		QSurfaceFormat fmt;
+		fmt.setRenderableType(QSurfaceFormat::OpenGL);
+		fmt.setVersion(3, 2);
+		fmt.setProfile(QSurfaceFormat::CoreProfile);
+		fmt.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+		fmt.setRedBufferSize(8);
+		fmt.setGreenBufferSize(8);
+		fmt.setBlueBufferSize(8);
+		fmt.setDepthBufferSize(24);
+		fmt.setStencilBufferSize(8);
+		fmt.setAlphaBufferSize(0);
+		fmt.setStereo(false);
+		fmt.setSamples(0);//vtkOpenGLRenderWindow::GetGlobalMaximumNumberOfMultiSamples());
+		QSurfaceFormat::setDefaultFormat (fmt);
+#endif	// #if (VTK_MAJOR_VERSION >= 9)
 		QtVtk::initialize ( );
 		QtViewPointWindow*	mainWindow	= new QtViewPointWindow ( );
 		mainWindow->show ( );
